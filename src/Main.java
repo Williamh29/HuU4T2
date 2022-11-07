116d1531e7e9bfbabd785c35925ff56263c60534
student <williamh29@nycstudents.net>
2022-11-07 11:02:42 -0500

    initialize

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+//        for (int count = 1; count <= 10; count++) {
+//            System.out.println(count + " ");
+//        }
+//        for (int count = 1; count < 11; count++) {
+//            System.out.println(count + " ");
+//        }
+//        for (int count = 15; count >= 0; count--) {
+//            System.out.println(count + " ");
+//        }
+//        for (int count = 15; count > -1; count--) {
+//            System.out.println(count + " ");
+//        }
+//        for (int count = 1; count <= 15; count += 2) {
+//            System.out.println(count + " ");
+//        }
+//        for (int count = 1; count < 16; count += 2) {
+//            System.out.println(count + " ");
+        for (int i = 5; i > 0; i++) {
+            System.out.println(i);
+        }
+        System.out.print("end");
+
+
+
+
+
+
+    }
+
+}
